2b25529d7c39f6b2a64db3ef6ec1050595712878
Aero_Bucket <jason303037@yahoo.com>
2018-05-20 16:40:42 +0800

    fixing duplicate subjects in electives field

--- subject_generator.py
+++ subject_generator.py
@@ -7,11 +7,11 @@ names=["Mike","James","May","Steve","Peter","Mary","George",
 surnames=["Lee","Wong","Ng","Chan","Ho",
           "Cheng","Tze","Kwok","Hong","Yuen"]
 
-x1=["None","economics_1","geography_1","chemistry_1","physics_1"]
+x1=["None","economics","geography","chemistry","physics"]
 
-x2=["bafs","biology_2","chistory","economics_2","ict","physics_2"]
+x2=["bafs","biology","chistory","economics","ict","physics"]
 
-x3=["biology_3","chemistry_3","cliterature","geography_3","history","va","m2"]
+x3=["biology","chemistry","cliterature","geography","history","va","m2"]
 
 all_subjects=["economics","geography","chemistry","physics","bafs","biology",
               "chistory","ict","cliterature","history","va","m2"]
@@ -20,8 +20,10 @@ fields=["name","average","x1","x2","x3",
         "pref1","pref2","pref3","pref4","pref5","pref6",
         "pref7","pref8","pref9","pref10","pref11","pref12"]
 
-prefs=["pref1","pref2","pref3","pref4","pref5","pref6",
+prefs_field=["pref1","pref2","pref3","pref4","pref5","pref6",
         "pref7","pref8","pref9","pref10","pref11","pref12"]
+
+electives_field=["x1","x2","x3"]
 
 
 with open("random_subject.csv","w") as rsub:
@@ -31,15 +33,27 @@ with open("random_subject.csv","w") as rsub:
     for n in names:
         for s in surnames:
             random.shuffle(all_subjects)
-            pref_dict=dict(zip(prefs,all_subjects))
+            pref_dict=dict(zip(prefs_field,all_subjects))
 
-            csv_dict={"name":n+" "+s,
+            breaker=True
+            while breaker:
+                first=random.choice(x1)
+                second=random.choice(x2)
+                third=random.choice(x3)
+                
+                if first != second != third:
+                    elective_list=[first,second,third]
+                    elective_dict=dict(zip(electives_field,elective_list))
+                    breaker=False
+                
+            csv_dict={"name":f"{n} {s}",
                       "average":round(random.uniform(0,100),2),
                       "x1":random.choice(x1),
                       "x2":random.choice(x2),
                       "x3":random.choice(x3),
                      }
             csv_dict.update(pref_dict)
+            csv_dict.update(elective_dict)
             
             writer.writerow(csv_dict)
 
